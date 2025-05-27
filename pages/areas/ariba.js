536b9ca0347d3d6bc6c47468eb1213a2e91b43ef
OtavioKashima <otavioyuukikashima1@gmail.com>
2025-05-27 11:13:25 -0300

    Certo

---
+++ pages/areas/ariba.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', async function() {
+    const area = "Aribabiba";
+    const token = localStorage.getItem('token');
+    const response = await fetch(`http://localhost:3000/brinquedos/${area}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'},
+            'Authorization': `Bearer`+token
+        });
+        const data = await response.json();
+        console.log(data);
+    });
+
